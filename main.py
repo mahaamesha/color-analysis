d4ad16b645c737d279d4a1ed3c8b874d99c81160
mahaamesha <mahaamesha@gmail.com>
2023-05-24 20:24:07 +0700

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+import mafunction as f
+from mafunction import input_folder_path
+
+
+
+if __name__ == '__main__':
+    for fname in os.listdir(input_folder_path)[1:]:
+        file_path = os.path.join(input_folder_path, fname)
+    
+        print(file_path)
+        f.im_process(file_path)
+        
+        # background_color = f.detect_background_color(file_path)
+        # print(f"Background Color (BGR): {background_color}")
+        # break
+        
